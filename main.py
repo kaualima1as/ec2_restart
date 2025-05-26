a0c66b19aa631a97b78e074d2ca7c1d22b9bf311
Kauã Lima <kaualimadesouza15@gmail.com>
2025-05-26 15:26:10 -0300

    fix: restarting EC2

--- main.py
+++ main.py
@@ -9,10 +9,24 @@ def handler(event, context):
 
     ec2 = boto3.client("ec2")
     try:
-        response = ec2.reboot_instances(InstanceIds=[instance_id])
-        return {
-            "body": f"Reboot initiated for instance {instance_id}. Response: {response}",
-            "statusCode": 200,
-        }
+        response = ec2.describe_instances(InstanceIds=[instance_id])
+        state = response["Reservations"][0]["Instances"][0]["State"]["Name"]
+
+        if state == "running":
+            response = ec2.reboot_instances(InstanceIds=[instance_id])
+            return {
+                "body": f"Reboot initiated for instance {instance_id}. Response: {response}",
+                "statusCode": 200,
+            }
+        elif state == "stopped":
+            return {
+                "body": f"Instance {instance_id} is stopped.",
+                "statusCode": 200,
+            }
+        else:
+            return {
+                "body": f"Instance {instance_id} is in '{state}' state. No action taken.",
+                "statusCode": 200,
+            }
     except Exception as e:
         return {"body": f"An error occurred: {str(e)}", "statusCode": 500}
